d2dea4d07109ba98cabdab556d5222e6b3d60fe8
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-02-01 12:20:19 +0100

    started project

--- Makefile
+++ Makefile
@@ -8,4 +8,4 @@ CXXFLAGS += -C:/Qt/6.4.2/mingw_64/include
 #-C:/Qt/6.4.2/mingw_64/include/QtCore -C:/Qt/6.4.2/mingw_64/include/QtGui -C:/Qt/6.4.2/mingw_64/include/QtWidgets 
 
 LDFLAGS += -C:/Qt/6.4.2/mingw_64/libs
-LDLIBS += -lQtCore -lQtGui -lQApplication -lPBushButton
+LDLIBS += -QtCore -QtGui -QApplication -PBushButton
--- src/main.cpp
+++ src/main.cpp
@@ -16,3 +16,33 @@ int main (int argc, char **argv)
 
     return app.exec(); //launchs event loop
 }
+
+/*  
+TODO:
+    - make an header containing all the keys, referred by pointers (maybe an array)
+
+PROBLEMS:
+    - how do i listen to system costantly?
+    - how do i input the system?
+    - how do i make it usable both in linux and windows?
+
+IDEAS:
+    - make code understand on which system its executed
+    - 
+
+PROJECT:
+    Headers:
+        - keyLayout.h
+            [ contains all key references for all the different layouts, maybe with pointers since they're fast af ]
+
+        -
+
+    Classes:
+        - Keyboard
+        - Macro
+
+    Methods:
+        - Macro.new()
+        - Macro.edit()
+        - Macro.delete()
+*/
