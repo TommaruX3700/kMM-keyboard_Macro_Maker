c72bcec1f07eb1b78d6eaa4cf32e351c2e719a00
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-07-24 16:44:34 +0200

    asd

--- files-kMM_Application/QtInterface/main.cpp
+++ files-kMM_Application/QtInterface/main.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+    /*
+     * 1 - Checks if kMM is already running
+     * 2 - If its not running,
+     */
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
